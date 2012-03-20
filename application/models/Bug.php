a3cf9515f38cab24b9bc7c47ed89aaff6f186937
David Sullivan <david@teamsullivango.com>
2012-03-20 17:08:56 -0400

    Added cms contentnode and page models. Holy Cow Pies Batman, I'm making a cms from scratch.

--- application/models/Bug.php
+++ application/models/Bug.php
@@ -1,17 +1,19 @@
 <?php
 class Model_Bug extends Zend_Db_Table_Abstract
 {
-// 	create table bugs (
-// 		id int(11) unsigned not null auto_increment comment 'Bug id number',
-// 		author varchar(250) default null comment 'author of the bug',
-// 		email varchar(250) default null comment 'email address of the author',
-// 		date int(11) default null comment 'date the bug occured',
-// 		url varchar(250) default null comment 'issue url',
-// 		description text comment 'description of bug symptomes',
-// 		priority varchar(50) default null comment 'the priority having this bug fixed',
-// 		status varchar (50) default null commnet 'the stage of this fix for this bug',
-// 		primary key (id)
-// 	)
+	/*
+	create table bugs (
+		id int(11) unsigned not null auto_increment comment 'Bug id number',
+		author varchar(250) default null comment 'author of the bug',
+		email varchar(250) default null comment 'email address of the author',
+		date int(11) default null comment 'date the bug occured',
+		url varchar(250) default null comment 'issue url',
+		description text comment 'description of bug symptomes',
+		priority varchar(50) default null comment 'the priority having this bug fixed',
+		status varchar (50) default null commnet 'the stage of this fix for this bug',
+		primary key (id)
+	)
+	*/
 	
 	
 	protected $_name = 'bugs';
---
+++ application/models/ContentNode.php
@@ -0,0 +1,46 @@
+<?php
+class Model_ContentNode extends Zend_Db_Table_Abstract
+{
+
+	/*
+	create table content_nodes (
+	id int(11) not null auto_increment,
+	page_id int(11) default null,
+	node varchar(50) default null,
+	content text,
+	primary key (id)
+	)default charset=utf8;
+	*/
+
+	protected $_name = 'content_nodes';
+	
+	protected $_referenceMap = array(
+		'Page' => array(
+			'columns'		=>	array('page_id'),
+			'refTableClass'	=>	'Model_Page',
+			'refColumns'	=>	array('id'),
+			'onDelete'		=> 	self::CASCADE,
+			'onUpdate'		=>	self::RESTRICT
+		)
+	);
+	
+	function setNode($pageId, $node, $content) 
+	{
+		//fetch it if it exists
+		$select = $this->select();
+		$select->where('page_id = ?', $pageId);
+		$select->where('node = ?', $node);
+		$row = $this->fetchRow($select);
+		
+		//if it doesn't then create it
+		if (!$row) 
+		{
+			$row = $this->createRow();
+			$row->page_id = $pageId;
+			$row->node = $node;
+		}
+		
+		$row->content = $content;
+		$row->save();		
+	}
+}
